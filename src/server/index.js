c775fda9bf3e7be3a4914135a7b7ff71543cb02d
ArthurFCouto <arthurfcouto@gmail.com>
2022-05-29 15:25:00 -0300

    Correção da porta de inicialização

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const server = require('./server');
 const config = require('./config');
 
-const { port, host } = config;
-server.listen(port, host, ()=> {
-  console.log(`Server is running on http://${host}:${port}`);
+const { port } = config;
+server.listen(port, ()=> {
+  console.log('Server is running');
 });
